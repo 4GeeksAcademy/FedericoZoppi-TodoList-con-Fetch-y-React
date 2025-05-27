bb1a05a57ec6d6e274984c7a0457d1b73aa19c74
FEDERICO ZOPPI <federicozoppi.ita@gmail.com>
2025-05-27 19:36:46 +0000

    le agregue el boton de recargar

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -8,13 +8,18 @@ const TodoList = () => {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
 
-  // Cargar tareas al iniciar
-  useEffect(() => {
-    fetch(GET_URL)
+const getTodos = () => {
+  fetch(GET_URL)
       .then(res => res.json())
       .then(data => setTodos(Array.isArray(data.todos) ? data.todos : []))
       .catch(error => console.error("Error al cargar tareas:", error));
+}
+  // Cargar tareas al iniciar
+  useEffect(() => {
+    getTodos();
+   
   }, []);
+
 
   const reloadTasks = () => {
     fetch(GET_URL)
@@ -24,6 +29,7 @@ const TodoList = () => {
 
   const addTask = () => {
     if (input.trim() === "") return;
+
 
     fetch(POST_URL, {
       method: "POST",
@@ -35,6 +41,8 @@ const TodoList = () => {
         reloadTasks();
       });
   };
+  
+  
 
   const deleteTask = (id) => {
     fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
@@ -52,26 +60,29 @@ const TodoList = () => {
     ).then(() => setTodos([]));
   };
 
+  
+
+
   return (
     <div className="todo-container">
       <h1>📝 TODO List</h1>
       <div className="input-section row">
-  <div className="col-12 col-sm-8">
-    <input
-      type="text"
-      value={input}
-      onChange={e => setInput(e.target.value)}
-      onKeyDown={e => e.key === "Enter" && addTask()}
-      placeholder="Nueva tarea..."
-      className="form-control"
-    />
-  </div>
-  <div className="col-12 col-sm-4">
-    <button onClick={addTask} className="btn btn-primary w-100">
-      Agregar
-    </button>
-  </div>
-</div>
+        <div className="col-12 col-sm-8">
+          <input
+            type="text"
+            value={input}
+            onChange={e => setInput(e.target.value)}
+            onKeyDown={e => e.key === "Enter" && addTask()}
+            placeholder="Nueva tarea..."
+            className="form-control"
+          />
+        </div>
+        <div className="col-12 col-sm-4">
+          <button onClick={addTask} className="btn btn-primary w-100">
+            Agregar
+          </button>
+        </div>
+      </div>
       <ul>
         {todos.length === 0 && <li className="empty">No hay tareas</li>}
         {todos.map(todo => (
@@ -80,7 +91,14 @@ const TodoList = () => {
             <button onClick={() => deleteTask(todo.id)}>❌</button>
           </li>
         ))}
+        <li>
+          <button onClick={getTodos} className="btn btn-secondary">
+            Recargar
+          </button>
+        </li>
       </ul>
+
+      
 
       {todos.length > 0 && (
         <button className="clear" onClick={clearAll}>
@@ -88,7 +106,9 @@ const TodoList = () => {
         </button>
       )}
     </div>
-  );
+    
+  )
+  ;
 };
 
 export default TodoList;
